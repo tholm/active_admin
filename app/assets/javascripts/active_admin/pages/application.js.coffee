14b4cad832251b6c54b07dad92f2b992b921ee38
Viktor Fonic <viktor.fonic@gmail.com>
2013-09-12 16:31:48 +0200

    Update action buttons; Revert application.js.coffee to previous version

--- app/assets/javascripts/active_admin/pages/application.js.coffee
+++ app/assets/javascripts/active_admin/pages/application.js.coffee
@@ -1,18 +1,11 @@
-# Initializers
+#
+# Active Admin JS
+#
+
+
 $ ->
-  # jQuery datepickers (also evaluates dynamically added HTML)
-  $(document).on 'focus', '.datepicker:not(.hasDatepicker)', ->
-    $(@).datepicker dateFormat: 'yy-mm-dd'
-
-  # Clear Filters button
-  $('.clear_filters_btn').click ->
-    window.location.search = ''
-
-  # Batch Actions dropdown
-  $('.dropdown_button').popover()
-  # Filter form: don't send any inputs that are empty
-  $('#q_search').submit ->
-    $(@).find(':input').filter(-> @value is '').prop 'disabled', true
-
-  # Tooltips
-  $('.member_link').tooltip()
+  # Date picker
+  $(".datepicker").datepicker format: "yyyy-mm-dd"
+  $(".clear_filters_btn").click ->
+    window.location.search = ""
+    false
--- lib/active_admin/views/index_as_table.rb
+++ lib/active_admin/views/index_as_table.rb
@@ -188,13 +188,13 @@ module ActiveAdmin
           links = proc do |resource|
             links = []
             if controller.action_methods.include?('show') && authorized?(ActiveAdmin::Auth::READ, resource)
-              links << link_to("<i class='icon-eye-open'></i>".html_safe, resource_path(resource), :title => I18n.t('active_admin.view'), :class => "member_link view_link")
+              links << link_to(I18n.t('active_admin.view'), resource_path(resource), :class => "member_link view_link", :title => I18n.t('active_admin.view'))
             end
             if controller.action_methods.include?('edit') && authorized?(ActiveAdmin::Auth::UPDATE, resource)
-              links << link_to("<i class='icon-edit'></i>".html_safe, edit_resource_path(resource), :title => I18n.t('active_admin.edit'), :class => "member_link edit_link")
+              links << link_to(I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link", :title => I18n.t('active_admin.edit'))
             end
             if controller.action_methods.include?('destroy') && authorized?(ActiveAdmin::Auth::DESTROY, resource)
-              links << link_to("<i class='icon-trash'></i>".html_safe, resource_path(resource), :method => :delete, :data => {:confirm => I18n.t('active_admin.delete_confirmation')}, :class => "member_link delete_link", :title => I18n.t('active_admin.delete'))
+              links << link_to(I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :data => {:confirm => I18n.t('active_admin.delete_confirmation')}, :class => "member_link delete_link", :title => I18n.t('active_admin.delete'))
             end
             links.join(' ').html_safe
           end
